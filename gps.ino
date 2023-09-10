c0208e34b0b7247469cfb37d6790cc6f15f623fc
Vsarkar018 <sarkarvishal018@gmail.com>
2023-09-10 16:07:40 +0530

    GY-GPS6MV2

--- gps.ino
+++ gps.ino
@@ -1,22 +1,29 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
+
 SoftwareSerial gpsSerial(0, 1); // RX, TX pins for GPS module
 TinyGPSPlus gps;
 void setup() {
-  Serial.begin(9600); // initialize serial port for debugging
+
+  // Serial.begin(9600); // initialize serial port for debugging
   gpsSerial.begin(9600); // initialize GPS serial port
   // send any necessary configuration commands to the GPS module
 }
 
 void loop() {
   if (gpsSerial.available() > 0) {
-    // Serial.write(gpsSerial.read());
+    //  Serial.write(gpsSerial.read());
     gps.encode(gpsSerial.read());// Parse the input from the GY-GPS6MV2 
-   if (gps.location.isUpdated()){
-     Serial.print("Latitude= "); 
-     Serial.print(gps.location.lat(), 6);
-     Serial.print(" Longitude= "); 
-     Serial.println(gps.location.lng(), 6);
-   }
+    Serial.print("Latitude= "); 
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(" Longitude= "); 
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("Altitude = "); 
+    Serial.println(gps.altitude.meters());
+    Serial.print("Time = "); 
+    Serial.println(gps.time.second());
+    Serial.print("Speed = "); 
+    Serial.println(gps.speed.kmph());
   }
+  delay(300);
 }
